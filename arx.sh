215db0630adcdb60c57431858843b0d016ea234e
442312 <1768438@gmail.com>
2025-05-14 00:00:53 +0300

    services test6

--- arx.sh
+++ arx.sh
@@ -102,7 +102,7 @@ export arxdotdir="$(pwd)/dots"
 ./scripts/02-00-nfs
 # ./scripts/02-01-wireguard
 # ./scripts/02-02-samba
-# ./scripts/02-03-tlp 
+./scripts/02-03-tlp 
 ./scripts/02-04-syncthing
 ./scripts/02-05-ssh
 
