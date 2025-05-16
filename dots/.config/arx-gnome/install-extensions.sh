98f32a005d5d86b9fe15eff69b90dcd5efe55abb
442312 <1768438@gmail.com>
2025-05-16 11:54:29 +0300

    moved gnome extensions to a separate script

---
+++ dots/.config/arx-gnome/install-extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 5. Install gnome shell extensions
+# - Install Caffeine extension
+~/.local/bin/gext install 517
+# - Install Just Perfiection extension
+~/.local/bin/gext install 3843
+# - Install Transparent Top Bar extension
+~/.local/bin/gext install 3960
+# - Install Run or Raise extension
+~/.local/bin/gext install 1336
